ca2ac3fd2d8933b4baae0634a3030dff267767c0
schmelczerandras <andras@schmelzer.dev>
2020-09-18 15:26:56 +0200

    Remove clutter

--- src/graphics/rendering/shaders/distance-fs.glsl
+++ src/graphics/rendering/shaders/distance-fs.glsl
@@ -12,58 +12,11 @@ in vec2 position;
 
 {declarations}
 
-/*
-#endif
-#if BLOB_COUNT > 0
-    uniform struct {
-        vec2 headCenter;
-        vec2 leftFootCenter;
-        vec2 rightFootCenter;
-        float headRadius;
-        float footRadius;
-        float k;
-    }[BLOB_COUNT] blobs;
-
-    float smoothMin(float a, float b)
-    {
-        const float k = 2.0;
-        
-        a = pow(a, k);
-        b = pow(b, k);
-        return pow((a * b) / (a + b), 1.0 / k);
-    }
-
-    float circleMinDistance(vec2 circleCenter, float radius) {
-        return distance(position, circleCenter) - radius;
-    }
-
-    void blobMinDistance(inout float minDistance, inout float color) {
-        for (int i = 0; i < BLOB_COUNT; i++) {
-            float headDistance = circleMinDistance(blobs[i].headCenter, blobs[i].headRadius);
-            float leftFootDistance = circleMinDistance(blobs[i].leftFootCenter, blobs[i].footRadius);
-            float rightFootDistance = circleMinDistance(blobs[i].rightFootCenter, blobs[i].footRadius);
-
-            float res = min(
-                smoothMin(headDistance, leftFootDistance),
-                smoothMin(headDistance, rightFootDistance)
-            );
-
-            res = min(100.0, headDistance);
-            res = min(res, leftFootDistance);
-            res = min(res, rightFootDistance);
-            //color = mix(2.0, color, step(distanceUvPixelSize + SURFACE_OFFSET, res));
-            minDistance = min(minDistance, res);
-            color = mix(2.0, color, step(distanceNdcPixelSize + SURFACE_OFFSET, res));
-        }
-    }
-#endif
-*/
-
 out vec2 fragmentColor;
 
 void main() {
     float minDistance = maxMinDistance;
-    float color = 1.0;
+    float color = 0.0;
 
     {functionCalls}
 
--- src/helper/rotate-90-deg.ts
+++
@@ -1,3 +0,0 @@
-import { vec2 } from 'gl-matrix';
-
-export const rotate90Deg = (vec: vec2): vec2 => vec2.fromValues(-vec.y, vec.x);
