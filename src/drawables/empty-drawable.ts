ca703c0131b0062dabd20d6fb8b9d88ab4c59b1c
schmelczerandras <schmelczer.andras@simonyi.bme.hu>
2020-11-17 10:27:04 +0100

    Fix linting

--- src/drawables/empty-drawable.ts
+++ src/drawables/empty-drawable.ts
@@ -10,10 +10,12 @@ import { DrawableDescriptor } from './drawable-descriptor';
 export class EmptyDrawable extends Drawable {
   public static readonly descriptor: DrawableDescriptor;
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public minDistance(target: vec2): number {
     throw new Error('Unimplemented');
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   protected getObjectToSerialize(transform2d: mat2d, transform1d: number): any {
     throw new Error('Unimplemented');
   }
